9d147970f02dcc1e6f11c26b41bbdde74ab309a1
Warizzi <2559188@students.wits.ac.za>
2025-06-01 20:37:57 +0200

    Form styling Test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import './App.css';
+
 import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
 import LogMeal from './Pages/LogMeal';
