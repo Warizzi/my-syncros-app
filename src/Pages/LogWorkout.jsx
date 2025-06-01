c7fa0c8deb89457ea90d1a844b189483e8f7a03d
Warizzi <2559188@students.wits.ac.za>
2025-06-01 21:37:04 +0200

    Logworkout and Style edits

--- src/Pages/LogWorkout.jsx
+++ src/Pages/LogWorkout.jsx
@@ -1,11 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const LogWorkout = () => {
+
+function LogWorkout() {
+    const [type, setType] = useState('');
+    const [duration, setDuration] = useState('');
+    const [notes, setNotes] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        //Basic validation
+        if (!type || !duration) {
+            setError('Workout type and duration are required.');
+            return;
+        }
+
+        if (isNaN(duration)) {
+            setError('Duration must be a number.');
+            return;
+        }
+
+        setError('');
+
+        //Should save to context API
+        console.log('Workout logged:', { type, duration, notes });
+
+        // Clear form
+        setType('');
+        setDuration('');
+        setNotes('');
+
+    };
+
     return (
         <div>
-            <h1>Welcome to your Health Tracker</h1>
+            <h2>Log your Workout</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="type">Workout Type:</label>
+                    <select
+                        id="type"
+                        value={type}
+                        onChange={(e) => setType(e.target.value)}>
+
+                        <option value="">-- Select Type --</option>
+                        <option value="Cardio">Cardio</option>
+                        <option value="Strength">Strength</option>
+                        <option value="Flexibility">Flexibility</option>
+                        <option value="HIIT">HITT</option>
+                        <option value="Other">Other</option>
+
+                    </select>
+                </div>
+
+                <div>
+                    <label htmlFor="duration">Duration (minutes):</label>
+                    <input 
+                    type="text"
+                    id="duration"
+                    value={duration}
+                    onChange={(e) => setDuration(e.target.value)}/>
+
+                </div>
+
+                <div>
+                    <label htmlFor="notes">Notes (optional):</label>
+                    <textarea
+                    id="notes"
+                    value={notes}
+                    onChange={(e) => setNotes(e.target.value)}/>
+                </div>
+
+                {error && <p style={{ color: 'red'}}>{error}</p>}
+                <button type="submit">Log Workout</button>
+            </form>
         </div>
     );
-};
+
+
+
+}
 
 export default LogWorkout;
